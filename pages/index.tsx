713a28bfb70836c679dea81a67528136105f0e14
Austin Biggs <austin@codesage.io>
2020-10-15 13:12:35 -0600

    Reorganize Home page and declard default export

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,20 @@
-import { Home } from "./home";
+import * as React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+import styles from "./home/style.m.scss";
+
+const Home = () => {
+  return (
+    <div className={styles.body}>
+      <Container>
+        <Row>
+          <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+            <></>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default Home;
