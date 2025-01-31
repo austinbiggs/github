b67fefdd72d4223000c50c061d03f99ea6eed60e
Austin Biggs <austintbiggs@gmail.com>
2025-01-31 13:34:22 -0600

    chore: use .env vars for constants

--- frontend/gql/constants.ts
+++ frontend/gql/constants.ts
@@ -1,4 +1,3 @@
-export const GITHUB_ENDPOINT = "https://api.github.com/graphql";
+export const GITHUB_ENDPOINT = process?.env?.NEXT_PUBLIC_GITHUB_GQL_ENDPOINT || "MISSING_GITHUB_GQL_ENDPOINT";
 
-// todo(austinbiggs): This token should be an env. variable and never checked into the Repo. However, for this project it's acceptable.
-export const GITHUB_TOKEN = "813cbfa0fdf055852b9c1cc31a0d58d21c86d2f1";
+export const GITHUB_TOKEN = process?.env?.NEXT_PUBLIC_GITHUB_TOKEN || "MISSING_GITHUB_TOKEN";
